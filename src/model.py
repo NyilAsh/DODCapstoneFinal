ae70c0568a67bdda245f4aa6328388ee7e606d36
Shah-Daniyal-Quadri <hj5430@wayne.edu>
2025-04-11 16:26:33 -0400

    updated model

--- src/model.py
+++ src/model.py
@@ -1,3 +1,4 @@
+# model.py
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -6,14 +7,16 @@ class DenseCNN(nn.Module):
     def __init__(self):
         super().__init__()
         
-        self.conv1 = nn.Conv2d(3, 8, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(11, 20, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(3, 4, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(7, 10, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(17, 4, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(21, 4, kernel_size=3, padding=1)
         
-        # Modified final layers
         self.flatten = nn.Flatten()
-        self.linear1 = nn.Linear(31 * 10 * 10, 100)  # 100 positions + 1 no-position class
-        # Removed unflatten and final conv layers
-
+        self.linear1 = nn.Linear(25*10*10, 250)  
+        self.linear2 = nn.Linear(250, 100)       
+        self.dropout = nn.Dropout(0.5) 
+                  
     def forward(self, x):
         out1 = F.relu(self.conv1(x))
         cat1 = torch.cat([x, out1], 1)
@@ -21,5 +24,13 @@ class DenseCNN(nn.Module):
         out2 = F.relu(self.conv2(cat1))
         cat2 = torch.cat([cat1, out2], 1)
         
-        x = self.flatten(cat2)
-        return self.linear1(x)  # Direct classification output
+        out3 = F.relu(self.conv3(cat2))
+        cat3 = torch.cat([cat2, out3], 1)
+        
+        out4 = F.relu(self.conv4(cat3))
+        cat4 = torch.cat([cat3, out4], 1)
+        
+        x = self.flatten(cat4)
+        x = F.relu(self.linear1(x))
+        x = self.dropout(x)
+        return self.linear2(x)
